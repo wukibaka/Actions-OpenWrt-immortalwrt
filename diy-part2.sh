e3e0c96594b15e5af304c61a1901cb738f00931d
wuki <51240031+wukibaka@users.noreply.github.com>
2024-05-02 20:56:14 +0800

    Initial commit

---
+++ diy-part2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.10.254/g' package/base-files/files/bin/config_generate
+
+rm -rf package/feeds/luci/luci-theme-argon
+rm -rf package/feeds/smallpackage/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/feeds/smallpackage/luci-theme-argon
